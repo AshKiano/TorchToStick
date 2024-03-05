3cfec7c7a7e98c32eacb4d89e6502c605fd73b8e
Josef Vyskoèil <josef.vyskocil@cleverlance.com>
2024-03-05 14:20:04 +0100

    init

---
+++ src/main/java/com/ashkiano/torchtostick/TorchToStick.java
@@ -0,0 +1,24 @@
+package com.ashkiano.torchtostick;
+
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TorchToStick extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event) {
+        if (event.getBlock().getType() == Material.TORCH) {
+            event.setDropItems(false);
+            event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.STICK));
+        }
+    }
+}
